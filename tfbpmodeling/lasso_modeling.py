17d3210a59e257cf18977669268eb085aa3e9657
chasem <chasem@wustl.edu>
2025-06-06 17:53:19 -0500

    removing sample weight cv logging

--- tfbpmodeling/lasso_modeling.py
+++ tfbpmodeling/lasso_modeling.py
@@ -1391,7 +1391,6 @@ def bootstrap_stratified_cv_modeling(
             logger.warning("Estimator does not have a random_state attribute.")
             pass
 
-        logger.info("Performing CV by sample weights")
         classes = stratification_classification(
             perturbed_tf_series.loc[bootstrapped_data.response_df.index].squeeze(),
             bootstrapped_data.response_df.squeeze(),
